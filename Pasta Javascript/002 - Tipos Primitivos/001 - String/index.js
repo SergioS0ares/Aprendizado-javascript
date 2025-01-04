7ad0b7d3f5ca940ebc8b2308d092c7fb2d346ebe
SergioS0ares <sergyn200295@gmail.com>
2025-01-04 14:40:38 -0300

    introdução a tipos primitivos String e manipulação com aspas
    Adicionado exemplos de como declarar strings em JavaScript:
    Usando aspas duplas (").
    Usando aspas simples (').
    Usando template strings (crase, `).
    Demonstrado como manipular strings contendo aspas e combinações de aspas:
    Exemplo de uso de aspas aninhadas e combinações variadas.
    Incluído conversão de números para string com String().
    Adicionado exemplo de comentários no código para explicar strings.

---
+++ Pasta Javascript/002 - Tipos Primitivos/001 - String/index.js
@@ -0,0 +1,15 @@
+console.log("Olá Mundo");
+console.log('Olá Mundo');
+console.log(`Olá Mundo`);
+
+console.log('"Texto com Aspas"');
+console.log(`"'"'""""""""""""'Texto com Aspas"""'''""""'"""""`)
+
+console.log(String(4654654656465));
+
+/*
+Como podemos escrever uma string 
+"Aqui vai uma sting"
+'Aqui vai uma sting'
+`Aqui vai uma sting`
+*/

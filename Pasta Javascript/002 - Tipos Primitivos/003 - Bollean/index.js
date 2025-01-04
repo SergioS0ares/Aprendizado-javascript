f83ffb68ae12ad4a0db80d482c9b6c10ed1bd733
SergioS0ares <sergyn200295@gmail.com>
2025-01-04 15:10:28 -0300

    introdução a tipos primitivos, arrays e objetos no JavaScript
    Números e conversões:
    Exemplos de uso do tipo Number e operações aritméticas.
    Conversão de strings para números com Number().
    Demonstração de possíveis armadilhas ao trabalhar com números em JavaScript.
    Booleanos:
    Introdução ao tipo Boolean com valores true e false.
    Exemplos de comparação de valores com o operador estrito (===).
    Valores especiais:
    Diferença entre undefined e null com exemplos práticos.
    Arrays:
    Exemplos de criação e manipulação de arrays.
    Uso de métodos como .length e .forEach() para percorrer elementos.
    Objetos:
    Exemplos de criação de objetos com propriedades e valores.
    Demonstração de objetos aninhados com propriedades detalhadas, como tênis, celular e drone.
    Representação de objetos em arrays para estruturar dados complexos.

---
+++ Pasta Javascript/002 - Tipos Primitivos/003 - Bollean/index.js
@@ -0,0 +1,17 @@
+console.log(typeof false);
+
+console.log("******");
+
+console.log(false);
+console.log(true);
+
+console.log("******");
+
+console.log("1" === 1);
+console.log(1 === 1);
+console.log(2 === 2);
+
+/*
+  Boolean é verdeiro ou falso
+  true / false
+*/
